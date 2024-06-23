ea52c8a4eae9f0b92f5fd5d943e7da75cb02c1c5
Joel Masiaga <masiagajoel001@gmail.com>
2024-06-23 21:12:43 +0300

    First commit

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+hours = 2
+minutes_per_hour = 60
+seconds_per_minute = 60
+seconds_per_hour = minutes_per_hour * seconds_per_minute
+seconds = hours * seconds_per_hour
+
+print(hours, "hour(s) is", seconds, "seconds." )
